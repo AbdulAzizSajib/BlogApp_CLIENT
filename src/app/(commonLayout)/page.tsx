10c190a705032923b7288360bafc6fa6255b7ead
MIS-SE-Abdul Aziz Sajib <Aziz.Sajib@aci-bd.com>
2026-01-18 15:25:17 +0600

    Update

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,17 +1,12 @@
 import { authClient } from '@/lib/auth-client';
-import { headers } from 'next/headers';
+
 export default async function Home() {
-  const session = await authClient.getSession({
-    headers: await headers(),
-  });
+  const session = await authClient.getSession();
 
   console.log('Session:', session);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h2>This is the Home page</h2>
-      <h2>
-          {session?.user ? `Hello, ${session.user.name}` : "Not logged in"}
-      </h2>
     </div>
   );
 }
