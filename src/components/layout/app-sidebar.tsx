e4ad981066978fb11dc069ec94b692afd33e4169
MIS-SE-Abdul Aziz Sajib <Aziz.Sajib@aci-bd.com>
2026-01-18 15:23:36 +0600

    Add Login & Register

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import { SearchForm } from '@/components/layout/search-form';
-import { VersionSwitcher } from '@/components/layout/version-switcher';
 import {
   Sidebar,
   SidebarContent,
